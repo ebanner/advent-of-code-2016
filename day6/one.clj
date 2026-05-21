093a99d76d620ff4cb8d2d2e740e3fc39d0464df
Edward Banner <edward.banner@gmail.com>
2026-05-21 19:08:51 -0400

    ★

---
+++ day6/one.clj
@@ -0,0 +1,38 @@
+(require '[clojure.string :as str])
+
+
+(defmacro reduce* [[acc init x coll] & body]
+  `(reduce (fn [~acc ~x] ~@body) ~init ~coll))
+
+
+(defn get-messages [input?]
+  (->>
+   (str/split-lines (or input? (slurp "input")))
+   (mapv vec)))
+
+
+(defn max-char [counts]
+  (->
+   (apply max-key val counts)
+   (key)))
+
+
+(defn get-counts [chars]
+  (reduce* [counts {}
+            c chars]
+    (update counts c (fnil inc 0))))
+
+
+(defn -main [& [input?]]
+  (let [messages (get-messages input?)
+        N (count messages)
+        M (count (first messages))
+        column (fn [j]
+                 (for [i (range N)] (get-in messages [i j])))]
+
+    (->>
+     (range M)
+     (map column)
+     (map get-counts)
+     (map max-char)
+     (apply str))))
