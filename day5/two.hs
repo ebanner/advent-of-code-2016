065c2b63e22b78116a86165a8cbcaac8f1a049c1
Edward Banner <edward.banner@gmail.com>
2026-05-21 09:41:10 -0400

    ⭐️

--- day5/two.hs
+++ day5/two.hs
@@ -1,7 +1,7 @@
-import Debug.Trace
-import Util (md5)
 import qualified Data.Map.Strict as Map
-import Data.Map.Strict (Map)
+import Data.Map.Strict (Map, (!))
+
+import Util (md5)
 
 
 getDoorId = getLine
@@ -18,27 +18,20 @@ getValue hash = hash !! 6
 update :: Map Int Char -> String -> Map Int Char
 update password hash = 
   let
-    (position, value) = trace ("hash=" ++ hash) $ (getPosition hash, getValue hash)
-    password' = if Map.member position password
-                   then password
-                   else Map.insert position value password
+    (position, value) = (getPosition hash, getValue hash)
   in
-    trace ("password=" ++ show password') $ password'
+    if Map.member position password
+       then password
+       else Map.insert position value password
 
 
 isValid :: String -> Bool
 isValid hash =
-  take 5 hash == "00000" && hash !! 5 `elem` ['0'..'7']
+  take 5 hash == "00000" &&hash !! 5 `elem` ['0'..'7']
 
 
-toString :: Map Int Char -> String
-toString chars =
-  let
-    password = foldl go [] [0..7]
-    go password i = password ++ [chars!i]
-  in
-    password
-
+getPassword :: Map Int Char -> String
+getPassword chars = map (chars!) [0..7]
 
 
 main = do
@@ -47,13 +40,12 @@ main = do
   let
     getKey n = doorId ++ show n
 
-    password = foldr go (const Map.empty) [0..] Map.empty
-    go n rest password
-      -- | trace ("n=" ++ show n ++ " password=" ++ show password) False = undefined
-      | length password == 8 = trace "1" $ password
-      | isValid hash = rest (update password hash)
-      | otherwise = rest password
+    chars = foldr go (const Map.empty) [0..] Map.empty
+    go n rest chars
+      | length chars == 8 = chars
+      | isValid hash = rest (update chars hash)
+      | otherwise = rest chars
 
       where hash = md5 $ getKey n
   in
-    toString password
+    getPassword chars
