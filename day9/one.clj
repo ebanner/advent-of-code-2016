5d0fcd10bd1af5faf0c62541dceb9672a68a9daf
Edward Banner <edward.banner@gmail.com>
2026-06-20 20:16:22 -0400

    🤔

--- day9/one.clj
+++ day9/one.clj
@@ -2,27 +2,37 @@
 
 
 (def clauses [])
-(defmacro defclause [name args pred body]
+(defmacro defclause [name args pred & body]
   `(do
-     (defn ~name ~args
+     (defn ~name [& args#]
        (loop [[clause# & rest#] clauses]
-         (if (apply (:pred clause#) ~args)
-           (apply (:body clause#) ~args)
+         (if (apply (:pred clause#) args#)
+           (apply (:body clause#) args#)
            (recur rest#))))
 
      (let [clause# {:pred (fn ~args ~pred)
-                    :body (fn ~args ~body)}]
+                    :body (fn ~args ~@body)}]
 
        (def clauses (conj clauses clause#)))))
 
 
 (defmacro reduce* [init [x coll] & body]
   (let [acc-names (map name (keys init))
-        acc-syms  (map symbol acc-names)]
-    `(reduce (fn [{:keys [~@acc-syms]} ~x]
-               ~@body)
-             ~init
-             ~coll)))
+        acc-syms  (map symbol acc-names)
+        has-result? (= (first body) :result)
+        result-expr (when has-result? (second body))
+        body-forms  (if has-result? (drop 2 body) body)]
+    (if has-result?
+      `(let [{:keys [~@acc-syms]}
+             (reduce (fn [{:keys [~@acc-syms]} ~x]
+                       ~@body-forms)
+                     ~init
+                     ~coll)]
+         ~result-expr)
+      `(reduce (fn [{:keys [~@acc-syms]} ~x]
+                 ~@body-forms)
+               ~init
+               ~coll))))
 
 
 (defn get-file [input?]
@@ -32,57 +42,64 @@
 
 
 (defn parse-marker [marker]
-  [1 1])
+  (let [toks (str/split marker #"x")]
+    (map Integer/parseInt toks)))
+
+
+(defn parse-chunk [[first & rest]]
+  (if (number? first)
+    [first (apply str rest)]
+    (apply str first rest)))
 
 
 (defn parse [file]
   (reduce* {:state :ELSE :chunks [] :chunk [] :ticks -1}
-      [c file]
+           [c file]
+           :result (if chunk (conj chunks (parse-chunk chunk)) chunks)
 
-      (letfn
-          [(handle-else []
-             (if (= c \()
-               {:state :IN-MARKER :chunks (conj chunks chunk) :chunk [] :ticks ticks}
-               {:state state :chunks chunks :chunk (conj chunk c) :ticks ticks}))
+    (letfn
+        [(handle-else []
+           (if (= c \()
+             {:state :IN-MARKER :chunks (conj chunks (apply str chunk)) :chunk [] :ticks ticks}
+             {:state state :chunks chunks :chunk (conj chunk c) :ticks ticks}))
 
-           (handle-in-marker []
-             (if (= c \))
-               (let [[ticks' _] (parse-marker chunk)]
-                 {:state :IN-CHARS :chunks chunks :chunk (conj chunk c) :ticks ticks'})
+         (handle-in-marker []
+           (if (= c \))
+             (let [[ticks' rep] (parse-marker (apply str chunk))]
+               {:state :IN-CHARS :chunks chunks :chunk [rep] :ticks ticks'})
+             {:state state :chunks chunks :chunk (conj chunk c) :ticks (dec ticks)}))
 
-               {:state state :chunks chunks :chunk (conj chunk c) :ticks (dec ticks)}))
+         (handle-in-chars []
+           (if (zero? ticks)
+             {:state :ELSE :chunks (conj chunks (parse-chunk chunk)) :chunk [c] :ticks -1}
+             {:state state :chunks chunks :chunk (conj chunk c) :ticks (dec ticks)}))]
 
-           (handle-in-chars []
-             (if (zero? ticks)
-               {:state :ELSE :chunks (conj chunks chunk) :chunk [] :ticks -1}
-               {:state state :chunks chunks :chunk (conj chunk c) :ticks ticks}))]
-
-        (case state
-          :ELSE (handle-else)
-          :IN-MARKER (handle-in-marker)
-          :IN-CHARS (handle-in-chars)))))
+      (case state
+        :ELSE (handle-else)
+        :IN-MARKER (handle-in-marker)
+        :IN-CHARS (handle-in-chars)))))
 
 
 (defclause expand [chunk] (string? chunk)
   chunk)
 
 
-(defclause expand [[_ rep] s] :else
- (->>
-  s
-  (repeat rep)
-  (apply str)))
-
-
-(defn decompress [file]
+(defclause expand [[rep s]] :else
   (->>
-   (parse file)
-   (map expand)
+   s
+   (repeat rep)
    (apply str)))
 
 
+(defn decompress [file]
+  (let [parsed (parse file)
+        expanded (map expand parsed)]
+
+    (apply str expanded)))
+
+
 (defn -main [& [input?]]
-  (->
-   (get-file input?)
-   (decompress)
-   (count)))
+  (let [file (get-file input?)
+        decompressed (decompress file)]
+
+    (count decompressed)))
